5e9d41f71b373d7fb1cd3c124470598bafa83e15
ColinHwang <chwang7158@gmail.com>
2016-07-04 18:36:10 +0800

    Initial Commit

---
+++ CHHTTPHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHHTTPHelper
+//
+//  Created by colin on 16/7/4.
+//  Copyright © 2016年 CHwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CHHTTPHelper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CHHTTPHelper
+//
+//  Created by colin on 16/7/4.
+//  Copyright © 2016年 CHwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
